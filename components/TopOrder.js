08a44dcc9b00fbacea0a27561036df9cb6b4c325
noritakasawada <snp.inc.info@gmail.com>
2021-11-04 12:36:04 +0900

    sixstate

--- components/TopOrder.js
+++ components/TopOrder.js
@@ -3,10 +3,8 @@ import React from 'react'
 const TopOrder = () => {
     return (
         <div className="flex justify-center">
-            <div className="text-xl lg:text-5xl flex  sm:my-0 lg:my-7 font-zen">
-                <h2>いっちゃが餃子6パック(10個入/パック)</h2>
-                <h3>3,700円</h3>
-                <span>税込</span>
+            <div className="text-xl lg:text-5xl flex flex-col  sm:my-0 lg:my-7 font-zen">
+                <h2>6パック3,700円<span>税込</span>(10個入/パック)</h2>
             </div>
 
         </div>
